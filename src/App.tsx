9bfc27045cd623a69ad4ac02e502ec3c76915093
RaphaelStopa <76446076+RaphaelStopa@users.noreply.github.com>
2022-04-14 18:46:52 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import { AuthProvider } from './contexts/AuthContext';
 import RouteApp from './utils/RouteApp';
 
 
-// Ele quer rotas em um arquivo separadfxFlexOffset
-// fiz a conta no firebase mais nao intregeu
-// coloque um pouco de css
-// header nao da auqi, tente colocar na arquivo de rota mesmo
-// https://www.youtube.com/watch?v=jCY6DH8F4oc em 15 minutos
-
 function App() {
   return (
     < AuthProvider>
